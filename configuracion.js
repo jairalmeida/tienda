fa544f72db15ef98728f192275aef497da04a13b
Jair Almeida <jartaam11@gmail.com>
2022-03-13 13:34:58 -0500

    Despliegue de aplicacion

---
+++ configuracion.js
@@ -0,0 +1,5 @@
+const config = {
+    DB_URL: 'mongodb+srv://jair:jairups@cluster0.sxmbv.mongodb.net/tiendadeber?retryWrites=true&w=majority',
+    PUERTO: process.env.PORT || 3000
+}
+module.exports = config
